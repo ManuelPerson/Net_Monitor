0a63e51498a443201175a794a75461e34da9fd02
ManuelPerson <127495647+ManuelPerson@users.noreply.github.com>
2023-07-17 01:25:03 +0200

    Update Net_Monitor4.5.py
    Update Net_Monitor4.5.py

--- Net_Monitor.py
+++ Net_Monitor.py
@@ -23,17 +23,24 @@ def packet_callback(packet):
         packet_type = ""
         if TCP in packet:
             packet_type = "TCP"
+            src_port = packet[TCP].sport
+            dst_port = packet[TCP].dport
         elif UDP in packet:
             packet_type = "UDP"
+            src_port = packet[UDP].sport
+            dst_port = packet[UDP].dport
         else:
             packet_type = "Other"
+            src_port = ""
+            dst_port = ""
 
         # Inkrementieren des Verkehrszählers
         traffic_counter += 1
 
         # Hinzufügen der Informationen in die Log-Datei
-        log_message = f"Empfangenes Paket: IP={src_ip} | Typ={packet_type}"
+        log_message = f"Empfangenes Paket: IP={src_ip} | Typ={packet_type} | Quellport={src_port} | Zielport={dst_port}"
         logging.info(log_message)
+
 
 # Funktion zum Überwachen des Netzwerkverkehrs
 def monitor_network_traffic():
@@ -41,16 +48,22 @@ def monitor_network_traffic():
 
     # Konfigurieren des Loggers
     logging.basicConfig(filename='lockdown.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+
+    #Begrüßung und Einleitung
     print('(C) 2023 Manuel Person "OpenPI Projekt"')
     print("")
     print("Willkommen beim Net-Monitor!")
+    print("Bitte lesen Sie aufmerksam die README.md- Datei.")
     print("")
-    print("Das Programm wird gestartet...")
+    print("Das Programm wird gleich gestartet...")
+    print("")
     print("Bitte gedulden Sie sich einige Minuten, das Programm ermittelt gerade den durchschnittlichen Netzwerkverkehr.")
     print("Sobald der durchschnittliche Netzwerkverkehr ermittelt wurde öffnet sich die Netzwerkressourcen-Anzeige.")
-    print("Bitte räumen Sie dem Programm die erforderlichen Rechte ein")
-    print("Bitte legen Sie dann hier in diesem Eingabefenster den Schwellenwert für die Scann- Sensitivität fest, sobald Sie dazu aufgefordert werden")
+    print("Bitte räumen Sie dem Programm die erforderlichen Rechte ein sobald Sie dazu aufgefordert werden sollten.")
+    print("Bitte legen Sie auch gleich in diesem Eingabefenster den Schwellenwert für die Scanner- Sensitivität fest,\n sobald Sie dazu aufgefordert werden.")
+    print("Nach dem Sie Ihren Schwellenwert definiert haben,\n wird das Programm mit einer kleinen Verzögerung hier in diesem Fenster mit der Messung beginnen.")
     print("\n")
+    print("Bitte haben Sie noch einen kleinen Augenblick Geduld.")
     print("\n")
 
     # Durchschnittlichen Netzwerkverkehr berechnen
@@ -78,14 +91,22 @@ def monitor_network_traffic():
     subprocess.Popen(["perfmon", "/res"])
 
     # Schwellenwert abfragen
-    print("Der Schwellenwert dient dazu, den aktuellen Netzwerkverkehr mit dem durchschnittlichen Netzwerkverkehr zu vergleichen und bei Bedarf eine Desktop- Warnmeldung auszugeben.")
-    print("Der Benutzer wird aufgefordert, einen Schwellenwert einzugeben der angibt,\n um wie viel Prozent der aktuelle Netzwerkverkehr den durchschnittlichen Netzwerkverkehr überschreiten muss, um eine Warnmeldung auszulösen.")
-    print("Wenn der aktuelle Netzwerkverkehr diesen Schwellenwert überschreitet, wird eine Benachrichtigung angezeigt, um auf eine potenzielle Netzwerküberlastung oder ungewöhnliche Aktivität hinzuweisen.")
+    print("Der Schwellenwert dient dazu, den aktuellen Netzwerkverkehr mit dem durchschnittlichen Netzwerkverkehr zu vergleichen\n und bei Bedarf eine Desktop- Warnmeldung auszugeben.")
+    print("Der Benutzer wird aufgefordert, einen Schwellenwert einzugeben der angibt,\n um wie viel Prozent der aktuelle Netzwerkverkehr den durchschnittlichen Netzwerkverkehr überschreiten muss,\n um eine Warnmeldung auszulösen.")
+    print("Wenn der aktuelle Netzwerkverkehr diesen Schwellenwert überschreitet, wird eine Benachrichtigung angezeigt,\n um auf eine potenzielle Netzwerküberlastung oder ungewöhnliche Aktivität hinzuweisen.")
     print("Durch die Eingabe des Schwellenwerts kann der Benutzer die Empfindlichkeit der Netzwerküberwachung anpassen.")
     print("Wir empfehlen je nach Art der Netzwerkumgebung einen Wert zwischen 5% und 25%.")
     print("\n")
-    threshold_input = input("Bitte geben Sie den Sensitiv-Schwellenwert zwischen 1% und 100% ein (ohne Prozentzeichen): ")
-    threshold = float(threshold_input)
+
+    threshold = None
+    while threshold is None:
+        threshold_input = input("Bitte geben Sie den Sensitiv-Schwellenwert zwischen 1 und 100 ein (empfohlen 5-25 %): ")
+        try:
+            threshold = int(threshold_input)
+            if not (1 <= threshold <= 100):
+                raise ValueError
+        except ValueError:
+            print("Ungültige Eingabe. Bitte geben Sie eine Zahl zwischen 1 und 100 ein.")
 
     # Erstellen der Reportdatei
     report_file = open("network_traffic_report.txt", "w")
@@ -112,21 +133,22 @@ def monitor_network_traffic():
         # Überprüfen, ob der aktuelle Netzwerkverkehr den Schwellenwert übersteigt
         if current_traffic > average_traffic * (1 + threshold / 100):
             # Benachrichtigung anzeigen
-            message = "Netzwerkverkehr übersteigt den Schwellenwert!"
-            notification.notify(title="Netzwerküberwachung", message=message, timeout=10)
+            message = " Der Netzwerkverkehr übersteigt den Schwellenwert!"
+            notification.notify(title="WARNUNG!", message=message, timeout=10)
 
             # Protokollieren der Aktivität in der Log-Datei
-            log_message = f"{'ACHTUNG WARNMELDUNG! ' if 'ACHTUNG WARNMELDUNG!' in message else ''}Aktueller Netzwerkverkehr: {current_traffic} | Durchschnitt: {average_traffic}"
+            log_message = f"{'Ich bin ein unnoetiger' if 'Bug im Code' in message else ''}Aktueller Netzwerkverkehr: {current_traffic} | Durchschnitt: {average_traffic}"
             logging.info(log_message)
 
         print(f"Aktueller Netzwerkverkehr: {current_traffic} pps")
+
+        # Daten in die Reportdatei schreiben
         report_file.write("Zeitpunkt: {} - Aktueller Netzwerkverkehr: {}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"), current_traffic))
-        report_file.flush()  # Daten in die Datei schreiben
-        time.sleep(1)
+        report_file.write("Schwellenwert in % angegeben: {}\n".format(threshold))
+        report_file.flush()
 
     # Reportdatei schließen
     report_file.close()
-
 
 
 # Netzwerkverkehr überwachen
